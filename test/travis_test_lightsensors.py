6a0922df10f95f2bda114125e2602002fe3ba419
sf <fjkks5is@engs.tamagawa.ac.jp>
2019-02-18 11:00:54 +0000

    add a new device

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -31,7 +31,7 @@ class LightsensorTest(unittest.TestCase):
 	def test_get_value(self):
 		rospy.set_param('lightsensors_freq',10)
 		time.sleep(2)
-		with open("/dev/rtlightsensors0","w") as f:
+		with open("/dev/rtlightsensor0","w") as f:
 		    f.write("-1 0 123 4321\n")
 
 		time.sleep(3)
