89093c5457f4ac2748a57ccfb4dbb2206c08f46d
sf <fjkks5is@engs.tamagawa.ac.jp>
2019-02-20 06:21:46 +0000

    Add retry

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -26,7 +26,7 @@ class LightsensorTest(unittest.TestCase):
 
 	def test_node_exist(self):
 		nodes = rosnode.get_node_names()
-		self.assertIn('/ligthsensors',nodes,"node does not exist")
+		self.assertIn('/lightsensors',nodes,"node does not exist")
 
 	def test_get_value(self):
 		rospy.set_param('lightsensors_freq',10)
