ebd6966a530aa5951a20bdb1bc654a3deb2b1c28
sf <fjkks5is@engs.tamagawa.ac.jp>
2019-03-05 07:02:22 +0000

    retry

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -13,19 +13,19 @@ class WallStopTest(unittest.TestCase):
 
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
-            left_hz = int(lf.readline().rstrip())
-            right_hz = int(rf.readline().rstrip())
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
 
         return left, right
 
     def test_io(self):
-        left_hz, right_hz = self.set_and_get(400,100,100,0)
+        left, right = self.set_and_get('400','100','100','0')
         self.assertTrue(left == 0 and right == 0,"cannot stop")
 
-        left_hz, right_hz = self.set_and_get(400,0,0,90)
+        left, right = self.set_and_get('400','0','0','90')
         self.assertTrue(left != 0 and right != 0,"cannot move again")
 
-        left_hz, right_hz = self.set_and_get(150,0,200,150)
+        left, right = self.set_and_get('150','0','200','150')
         self.assertTrue(left == 0 and right == 0,"cannot stop")
 
 if __name__ == '__main__':
